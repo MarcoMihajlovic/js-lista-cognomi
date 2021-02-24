f5169e7dc2cf626470428f2d95d3eea39ddb600d
Marco <marcosinisa.mihajlovic@gmail.com>
2021-02-24 16:02:37 +0100

    Mezzo Fix per Bonus

--- Bonus/js/main.js
+++ Bonus/js/main.js
@@ -1,15 +1,24 @@
 var cognomi = ["Bianchi", "Rossi", "Duzioni", "Balsano", "Verdi"];
 
+var cognomi2 = "";
+
 for(var i=0; i < cognomi.length; i++){
     document.getElementById("lista").innerHTML += "<li>" + cognomi[i] + "</li>"
 }
 
 function myFunction() {
+
+    for(var i=0; i < cognomi2.length; i++){
+        document.getElementById("lista-ordinata").innerHTML = ""
+    }
+
     var cognomeUtente = document.getElementById("cognome").value;
    
     cognomi.push(cognomeUtente);
 
     cognomi.sort();
+
+    cognomi2 = cognomi;
 
     for(var i=0; i < cognomi.length; i++){
         document.getElementById("lista-ordinata").innerHTML += "<li>" + cognomi[i] + "</li>"
